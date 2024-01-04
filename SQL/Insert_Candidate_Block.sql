4283ba49db65f908c0f9c53b0bac1bb2180498f6
Khoa Huynh-Trong <69152064+khoaht312@users.noreply.github.com>
2024-01-04 09:28:56 +0700

    Update Insert_Candidate_Block.sql

--- SQL/Insert_Candidate_Block.sql
+++ SQL/Insert_Candidate_Block.sql
@@ -1,9 +1,6 @@
-SELECT cb.candidate_id, cb.block_id, cb.score
-FROM Candidate_Block cb
-JOIN (
-    SELECT candidate_id, MAX(score) AS highest_score
-    FROM Candidate_Block
-    GROUP BY candidate_id
-) max_scores ON cb.candidate_id = max_scores.candidate_id AND cb.score = max_scores.highest_score
-order by score desc
-limit 10;
+INSERT INTO Candidate_Block (candidate_id, block_id, score)
+SELECT cs.candidate_id, bi.block_id, SUM(cs.score)
+FROM Candidate_Subject cs
+JOIN Block_Subject bs ON cs.subject_id = bs.subject_id
+JOIN Block_info bi ON bs.block_id = bi.block_id
+GROUP BY cs.candidate_id, bi.block_id;
